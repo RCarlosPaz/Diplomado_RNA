b47d7faaaeb4d954460bc61b34c129ff190663a7
RCarlosPaz <rcpaz.cursos@gmail.com>
2026-03-11 20:58:01 -0600

    Add files via upload
    Cargar Comentarios y Numeros

---
+++ Modulo2/Programas/tema1/Ejercicios/Ejercicio_Numeros.py
@@ -0,0 +1,19 @@
+nombre_astronauta = "Roberto Carlos Paz Guerrero"
+edad_astronauta = 36
+destino = "Saturno"
+combustible = 89
+Velocidad = 36000
+
+
+print(f'''Diario de un Astronauta
+
+Hola, soy {nombre_astronauta}, tengo {edad_astronauta} años y mi próximo destino es {destino}.
+Estoy navegando a {Velocidad} km/s con {combustible}% de combustible restante hacia {destino}.
+
+Fecha: 2026-03-05
+Hoy experimentamos la vista de los anillos de este planeta
+Mensaje personal: ¡Es increíble ver cómo se distinguen claramente los siete anillos!
+
+Fecha: 2026-03-06
+Atravesamos los siete anillos para acercarnos al planeta!
+Mensaje personal: Nuestra siguiente misión sera marte, antes de regresar a casa, el planeta tierra.''')
